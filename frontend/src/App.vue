de053cc4f09a7118fa8c4831ffec615aa9c477aa
Emmanuel Stocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-08-07 18:49:18 -0300

    chore: ajustes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,63 +1,62 @@
 <template>
-  <div id="app" class="app"> 
+  <div id="app" class="app">
     <nav>
       <div class="nav-wrapper blue darken-1">
         <a href="#" class="brand-logo center">M2 Carrinho de Compras </a>
       </div>
     </nav>
 
-    <content class="content">
-      <h3> Internet </h3>
-      <h5>Selecione um plano de internet para continuar </h5>
-      <!-- cards plano de internet -->
-      <scroll-view
-          v-for="(internet, index) in internetAll" :key="index"
-      >       
-          <CardInternet
-            :internet_prod="internet" 
-          />
-      </scroll-view>  
+    <content class="content">    
+      <div>
+         <h3> Telefone </h3>
+         <h5>Selecione um plano de internet para continuar </h5>
+          <scroll-view v-for="(phone, index) in phoneAll" :key="index">
+            <Card :prod="phone"   
+              />
+          </scroll-view>
+      </div>
 
-      <h3> Um plano de Tv é sempre bom!  </h3>
-      <h5>Agora escolha seu pacote de TV </h5>
-      <scroll-view v-for="(tv, index) in tvAll" :key="index">
-        <CardInternet
-          :internet_prod="tv" />
-      </scroll-view>
+      <div>
+         <h3> Internet </h3>
+          <h5>Selecione um plano de internet para continuar </h5>
+          <scroll-view v-for="(internet, index) in internetAll" :key="index">
+          <Card :prod="internet"   
+            />
+         </scroll-view>
+      </div>
 
-      <h3> Fixo </h3>
-      <h5>Agora escolha seu pacote de telefone fixo </h5>
-      <scroll-view v-for="(phone, index) in phoneAll" :key="index">
-        <CardInternet
-          :internet_prod="phone" />
-      </scroll-view>
+      <div>
+         <h3> TV </h3>
+          <h5>Selecione um plano de internet para continuar </h5>
+           <scroll-view v-for="(tv, index) in tvAll" :key="index">
+            <Card :prod="tv"   
+              />
+         </scroll-view>
+      </div>
 
-
-      <hr>
-
-      <h3> Taxa de instalação    - gratis</h3>
-      <h3> Total R$: 00/mês</h3>
-
-      <button> Continuar </button>
+      
     </content>
   </div>
 </template>
 
 <script>
- 
-import Product from './services/products'
-import CardInternet from '../src/components/CardInternet.vue'
 
+import Product from './services/products'; 
+import Card from '../src/components/Card.vue';
 export default {
   name: 'app',
 
   data() {
-    return { 
+    return {
       phoneAll: [],
       tvAll: [],
-      internetAll: []
+      internetAll: [],
+      cart: [],
+      
+      internetSelected: []
     }
-  }, 
+  },
+
 
   mounted() {
     this.listInternet();
@@ -65,8 +64,8 @@ export default {
     this.listPhones();
   },
 
-  components: {
-    CardInternet
+  components: { 
+    Card
   },
 
   methods: {
@@ -92,25 +91,27 @@ export default {
       }).catch(error => {
         console.log(error);
       })
-    },
+    }, 
 
-  } 
+    inCart(product) {
+            return this.cart.indexOf(product) != -1
+    },
+    
+  }
 }
 
 </script>
 
 <style>
-  .container {
-    flex-direction: row; 
-  }
+.container {
+  flex-direction: row;
+}
 
-  .content {  
-    display: inline-block
-  }
+.content {
+  display: inline-block;
+  align-items: center;
+  padding: 10px;
+}
 
-  .app { 
-    
-
-  }
-
+.app {}
 </style>
---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+      <button class="card" >
+        <h6> {{ prod.title }} </h6>
+        <p> {{ prod.price }}</p>
+
+
+         <button v-if="prod.type_item == 'internet'" :title=prod.description> - detalhes </button> 
+    </button>
+</template>
+
+<script> 
+export default ({
+    name: 'Card',
+    props: {
+        prod: {
+            type: Object,
+            required: false,
+            default: false,
+            itemType: String
+        },
+        prod_selected: {
+            type: Object,
+            required: false,
+            default: false,
+            itemType: String
+        }, 
+    } 
+})
+</script>
+
+
+<style>
+    .card {
+ box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  margin-left: 7.5px;
+  margin-right: 7.5px;
+  width: 200px;
+  height: 200px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+</style>
